8202d33da5df115a67c35d3fb5c5644b733f839f
laughing-nerd <riturajpaul99@gmail.com>
2024-03-18 00:55:10 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/laughing-nerd/hehe
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		w.Write([]byte("Hello World!"))
+	})
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
